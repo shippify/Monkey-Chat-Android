543235924cbf0841bb16d8dd86bd847872f696eb
Gabriel Aumala <gabrielfima@live.com>
2016-08-18 17:18:36 -0500

    Retain data with fragment on configuration change
    ChatDataFragment class stores MainActivity's data onConfigurationChange.
    reorganize MainActivity's interface methods.
    
    MessageLoader class was modified to be easier to retain. Messages are now loaded
    before instantiating the chat fragment. MessageLoader has a new inner class
    MessageCounter that keeps track of the number of loaded messages. The
    RealmChangeListener was moved to a inner class NewMessagesListener.
    In addition to the loadNewPage method, there is also a loadFirstPage, which
    instantiates the Chat fragment after loading the first batch of messages.

---
+++ app/src/main/java/com/criptext/monkeychatandroid/ChatDataFragment.java
@@ -0,0 +1,24 @@
+package com.criptext.monkeychatandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.criptext.monkeychatandroid.models.MessageLoader;
+import com.criptext.monkeykitui.recycler.MonkeyItem;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+/**
+ * Created by gesuwall on 8/17/16.
+ */
+public class ChatDataFragment extends Fragment{
+    HashMap<String, Collection<MonkeyItem>> chatMap;
+    MessageLoader messageLoader;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+}
